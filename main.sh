c6a28ad262cfcbed45ec164015532732426b1a1a
viman <lukasz.bendykowski@gmail.com>
2017-06-01 12:25:56 +0200

    Sectioned off zsh opts to separate file

--- main.sh
+++ main.sh
@@ -15,48 +15,7 @@ export HISTSIZE=500000
 export HISTFILE="$HOME/.history"
 export SAVEHIST=$HISTSIZE
 
-# Remove superfluous blanks from each command line being added to the history
-# list
-setopt histreduceblanks
-# Remove command lines from the history list when the first character on the
-# line is a space, or when one of the expanded aliases contains a leading space
-setopt histignorespace
-# Do not enter command lines into the history list if they are duplicates of the
-# previous event.
-setopt histignorealldups
-# Switching directories for lazy people
-setopt autocd
-# See: http://zsh.sourceforge.net/Intro/intro_6.html
-setopt autopushd pushdminus pushdsilent pushdtohome pushdignoredups
-# Don't kill background jobs when I logout
-setopt nohup
-# See: http://zsh.sourceforge.net/Intro/intro_2.html
-setopt extendedglob
-# Do not require a leading '.' in a filename to be matched explicitly
-setopt globdots
-# Use vi key bindings in ZSH
-setopt vi
-# Causes field splitting to be performed on unquoted parameter expansions
-setopt shwordsplit
-# Automatically use menu completion after the second consecutive request for
-# completion
-setopt automenu
-# If the argument to a cd command (or an implied cd with the AUTO_CD option set)
-# is not a directory, and does not begin with a slash, try to expand the
-# expression as if it were preceded by a '~'
-setopt cdablevars
-# Try to make the completion list smaller (occupying less lines) by printing
-# the matches in columns with different widths
-setopt listpacked
-# Don't show types in completion lists
-setopt nolisttypes
-# If a completion is performed with the cursor within a word, and a full
-# completion is inserted, the cursor is moved to the end of the word
-setopt alwaystoend
-# Try to correct the spelling of commands
-setopt correct
-# https://github.com/robbyrussell/oh-my-zsh/issues/449
-setopt no_nomatch  
+source ~/.zsh/zshopts.sh
 
 # case-insensitive (uppercase from lowercase) completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
@@ -94,4 +53,3 @@ source ~/.zsh/aliases.sh
 
 ### PATH
 export PATH=/usr/local/bin:/usr/sbin:/sbin:/usr/bin:/bin
-[[ -s "/Users/richard/.gvm/scripts/gvm" ]] && source "/Users/richard/.gvm/scripts/gvm"
