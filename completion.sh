d1d4f203f0a0f557e60b56f94c803cc6f9471975
viman <lukasz.bendykowski@gmail.com>
2017-06-01 12:52:21 +0200

    Section off completion to a separate file

---
+++ completion.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+# Case-insensitive (uppercase from lowercase) completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# Process completion
+zstyle ':completion:*:processes' command 'ps -au$USER'
+zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=36=31"
+
+# Zstyle
+zstyle ':completion:*' completer _expand _complete _ignored _approximate
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' menu select=2
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*:descriptions' format '%U%F{yellow}%d%f%u'
+zstyle ':completion:*:*:git:*' script ~/.git-completion.sh
+
+# Completion
+autoload -U promptinit && promptinit
+autoload -U compinit compdef && compinit
--- main.sh
+++ main.sh
@@ -13,29 +13,11 @@ export HISTSIZE=500000
 export HISTFILE="$HOME/.history"
 export SAVEHIST=$HISTSIZE
 
+# Zsh options
 source $HOME/.zsh/zshopts.sh
 
-# case-insensitive (uppercase from lowercase) completion
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# process completion
-zstyle ':completion:*:processes' command 'ps -au$USER'
-zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=36=31"
-
-# zstyle
-zstyle ':completion:*' completer _expand _complete _ignored _approximate
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' menu select=2
-zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-zstyle ':completion:*:descriptions' format '%U%F{yellow}%d%f%u'
-zstyle ':completion:*:*:git:*' script ~/.git-completion.sh
-
-# Functions Autoloading
-fpath=($HOME/.zsh $fpath)
-
 # Completion
-autoload -U promptinit && promptinit
-autoload -U compinit compdef && compinit
+source $HOME/.zsh/completion.sh
 
 # Prompt
 prompt pure
